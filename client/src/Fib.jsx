7da73273291eb1b0fd2eff19dbd1dd3e97eba187
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-05-26 23:10:57 -0500

    dev working

--- client/src/Fib.jsx
+++ client/src/Fib.jsx
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 export default () => {
-    const [seenIndexes, setSeenIndexes] = useState();
-    const [values, setValues] = useState();
-    const [index, setIndex] = useState();
+    const [seenIndexes, setSeenIndexes] = useState([]);
+    const [values, setValues] = useState({});
+    const [index, setIndex] = useState('');
 
 
-    const fetchValues = async () => {
-        const values = await axios.get("/api/values/current")
-        setValues(values);
-    }
-
-    const fetchIndexes = async () => {
-        const indexes = await axios.get('/api/values/all');
-        setSeenIndexes(indexes);
-    }
 
     useEffect(() => {
+        const fetchValues = async () => {
+            const { data } = await axios.get("/api/values/current")
+            setValues(data);
+        }
+
+        const fetchIndexes = async () => {
+            const { data } = await axios.get('/api/values/all');
+            setSeenIndexes(data);
+        }
+
         fetchValues();
         fetchIndexes();
     }, []);
@@ -35,7 +36,7 @@ export default () => {
             )
         }
 
-        return values;
+        return entries;
     }
 
     const handleSubmit = async (event) => {
@@ -44,6 +45,7 @@ export default () => {
         await axios.post('/api/values', {
             index
         });
+
         setIndex('')
     }
 
@@ -56,7 +58,7 @@ export default () => {
             </form>
             <h3>
                 Indexes I've seen
-             {renderSIS}
+             {renderSIS()}
             </h3>
             <h3>
                 Calculated values:
--- server/index.js
+++ server/index.js
@@ -45,6 +45,8 @@ app.get("/", async(req, res) => {
 app.get("/values/all", async(req, res) => {
     const values = await pgClient.query("SELECT * from values");
 
+    console.log(values.rows);
+
     return res.send(values.rows);
 });
 
